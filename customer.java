3051935d96c1948809548d77990601fa5ae28fd5
Srujana <sujana234baluvu@gmail.com>
2022-02-17 11:00:04 +0530

    java file added

---
+++ customer.java
@@ -0,0 +1,44 @@
+public class customer{
+	private int id;
+	private String fname;
+	private String lname;
+	private String city;
+	private String country;
+	private Long phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Long getPhone() {
+		return phone;
+	}
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+}
